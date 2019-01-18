9b644cbc3768a92559a12940290f8479deeff1e3
Aurélien Devaux <hello@aureldvx.fr>
2019-01-18 01:02:54 +0100

    MAJ Gulp : ajout imageOptim et minifyJs

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,20 @@ const { src, dest, watch, parallel, series } = require ("gulp");
 const sass = require('gulp-sass'); // Needed to run SCSS/Sass preprocessor
 const browserSync = require('browser-sync').create(); // Needed to run Live Reload
 const imagemin = require('gulp-imagemin'); // Needed to Optimize Images
-const cache = require('gulp-cache'); // Speeds up image optimze
 const connect = require('gulp-connect-php');
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
 const sourcemaps = require("gulp-sourcemaps");
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const setDefaultBrowser = require('set-default-browser');
 
 
 // Browser Sync Start
 function browserSyncInitialization() {
+    setDefaultBrowser("Firefox Developer Edition");
     connect.server({}, function () {
         browserSync.init({
             proxy: "127.0.0.1:8888/woazoo/public/index.php/listing"
@@ -66,15 +70,41 @@ exports.exportCss = exportCss;
 
 // Image Cache Optimizer
 function imageOptimization() {
-    return src('images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(imagemin())
-        // Caching images that ran through imagemin
-        .pipe(cache(imagemin({
-            interlaced: true
-        })))
-        .pipe(dest('/public/img'));
+    return (
+        src('./src/Repository/img/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins : [
+                    {removeComments: true},
+                    {removeEmptyAttrs: true},
+                    {removeDesc: true},
+                    {removeTitle: true},
+                    {removeViewBox: false}
+                ]
+            })
+        ],{verbose: true}
+        ))
+        .pipe(dest('./public/img'))
+    );
 }
 exports.imageOptimization = imageOptimization;
+
+
+// Concatenate & minify Js
+function exportJs() {
+    return (
+        src("./src/Repository/js/**/*.js")
+        .pipe(sourcemaps.init())
+        .pipe(jshint())
+        .pipe(babel())
+        .pipe(sourcemaps.write())
+        .pipe(dest("./public/js"))
+    );
+}
+exports.exportJs = exportJs;
 
 
 function watching() {
@@ -85,6 +115,6 @@ function watching() {
 
     watch("./src/Repository/sass/**/*.scss", exportCss);
     watch("./templates/**/*.html.twig", browserSyncReload);
-    watch('./src/Repository/img/*.+(png|jpg|jpeg|gif|svg)', imageOptimization);
+    watch('./src/Repository/img/**/*.*', series(imageOptimization, browserSyncReload));
 }
 exports.watch = watching;
